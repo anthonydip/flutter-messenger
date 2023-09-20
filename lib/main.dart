108acefb8a80f9471a8978ebcab0d61247e138b4
Anthony Dip <anthonydip2001@gmail.com>
2023-09-20 18:32:01 -0400

    feat: friends and message screen routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-final List<int> _items = List<int>.generate(51, (int index) => index);
+class Friend {
+  final String name;
+
+  const Friend(this.name);
+}
 
 void main() {
   runApp(const MyApp());
@@ -21,75 +24,94 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
-        home: const MessagePage(),
+        home: const FriendsPage(),
       ),
     );
   }
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  
 }
 
-class MessagePage extends StatefulWidget {
-  const MessagePage({super.key});
-
-  @override
-  State<MessagePage> createState() => _MessagePageState();
-}
-
-class _MessagePageState extends State<MessagePage> {
-  double? scrolledUnderElevation;
+class FriendsPage extends StatelessWidget {
+  const FriendsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
-    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Friends List'),
+        shadowColor: Theme.of(context).colorScheme.shadow,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add, size: 30),
+            onPressed: () { 
+              // Add friend functionality
+              // code...
+             },
+          )
+        ],
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 10),
+        child: ListView(
+          children: [
+            for (var i = 1; i <= 100; i++)
+              ListTile(
+                title: Text('Friend $i'),
+                trailing: const Icon(Icons.message),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MessagePage(friend: Friend('Name $i')),
+                    )
+                  );
+                }
+              )
+          ]
+        ),
+      )
+    );
+  }
+}
+
+// class MessagePage extends StatefulWidget {
+//   const MessagePage({super.key, required this.friend});
+
+//   final Friend friend;
+
+//   @override
+//   State<MessagePage> createState() => _MessagePageState();
+// }
+
+class MessagePage extends StatelessWidget {
+  const MessagePage({super.key, required this.friend});
+
+  final Friend friend;
+
+  @override
+  Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Simple Messenger'),
-        scrolledUnderElevation: scrolledUnderElevation,
+        title: Text(friend.name),
         shadowColor: Theme.of(context).colorScheme.shadow,
         leading: Builder(
           builder: (BuildContext context) {
             return IconButton(
               icon: const Icon(Icons.arrow_back_ios),
-              onPressed: () {  },
+              onPressed: () { 
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FriendsPage()),
+                );
+              },
             );
           }
         )
-      ),
-      body: GridView.builder(
-        itemCount: _items.length,
-        padding: const EdgeInsets.all(8.0),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: 2.0,
-          mainAxisSpacing: 10.0,
-          crossAxisSpacing: 10.0,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          if (index == 0) {
-            return Center(
-              child: Text(
-                'Scroll to see the Appbar in effect.',
-                style: Theme.of(context).textTheme.labelLarge,
-                textAlign: TextAlign.center,
-              ),
-            );
-          }
-          return Container(
-            alignment: Alignment.center,
-            // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              color: _items[index].isOdd ? oddItemColor : evenItemColor,
-            ),
-            child: Text('Item $index'),
-          );
-        },
       ),
     );
   }
