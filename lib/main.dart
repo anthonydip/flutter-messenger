a8fcfc41e7fa752f4e0c7d0e6b13cacb48df6691
Anthony Dip <anthonydip2001@gmail.com>
2023-09-22 21:38:09 -0400

    feat: firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:simple_messenger/pages/auth_page.dart';
+import 'firebase_options.dart';
 
-import 'pages/login_page.dart';
 import 'components/friend.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -21,7 +27,7 @@ class MainApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
-        home: const LoginPage(),
+        home: const AuthPage(),
       ),
     );
   }
