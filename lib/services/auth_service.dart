6da7581d8db83800f9ab162620b0979da26414e7
Anthony Dip <anthonydip2001@gmail.com>
2023-10-19 15:26:32 -0400

    feat: add user to firestore database

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:simple_messenger/models/user.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AuthService {
+
+  // Add the user to the database
+  Future<void> addUserToDatabase(String email, String password, bool isFlutter) async {
+    // Make a POST request to create a new user
+    final response = await http.post(
+      Uri.parse('${dotenv.env['API_URL']}/users'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ${dotenv.env['INTERNAL_TOKEN']}'
+      },
+      body: jsonEncode(<String, String>{
+        'email': email,
+        'provider': isFlutter ? 'Flutter' : 'Google',
+        'password': password
+      }),
+    );
+
+    print(jsonDecode(response.body));
+
+    final body = jsonDecode(response.body);
+    final status = body['status'];
+
+    switch (status) {
+      case 'BAD REQUEST':
+        throw 'Invalid request';
+      case 'CONFLICT':
+        throw 'User already exists';
+      case 'INTERNAL SERVER ERROR':
+        throw 'Internal server error';
+    }
+  }
 
   // Google Sign In
   signInWithGoogle() async {
