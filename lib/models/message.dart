9dfee76784a15a98b81155ef3acb738cf35f8fa0
Anthony Dip <anthonydip2001@gmail.com>
2023-11-17 15:50:15 -0500

    feat: message decoration

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,6 +1,14 @@
 class Message {
   final String user;
   final String msg;
+  final String datetime;
 
-  const Message(this.user, this.msg);
+  const Message(this.user, this.msg, this.datetime);
+
+  @override
+  bool operator ==(Object other) =>
+    identical(this, other) || other is Message && other.user == user && other.msg == msg && other.datetime == datetime;
+
+  @override
+  int get hashCode => user.hashCode;
 }
